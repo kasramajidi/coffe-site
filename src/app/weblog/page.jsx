a4eb5ef653884381ea3c857d6a5b16ba48d65af0
kasra <kasramajdy81@gmail.com>
2024-05-15 23:09:15 +0330

    cahnge and currect header

--- src/app/weblog/page.jsx
+++ src/app/weblog/page.jsx
@@ -1,76 +1,8 @@
-import React from 'react';
-import Image from "next/image";
-import Link from "next/link";
+import React from 'react'
 
-import SportsEsportsOutlinedIcon from '@mui/icons-material/SportsEsportsOutlined';
-import InboxOutlinedIcon from '@mui/icons-material/InboxOutlined';
-import SpaOutlinedIcon from '@mui/icons-material/SpaOutlined';
-import PrecisionManufacturingOutlinedIcon from '@mui/icons-material/PrecisionManufacturingOutlined';
-import HomeWorkOutlinedIcon from '@mui/icons-material/HomeWorkOutlined';
-import TheaterComedyOutlinedIcon from '@mui/icons-material/TheaterComedyOutlined';
-import MenuBookOutlinedIcon from '@mui/icons-material/MenuBookOutlined';
-import ProductionQuantityLimitsOutlinedIcon from '@mui/icons-material/ProductionQuantityLimitsOutlined';
-import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
-import SmsOutlinedIcon from '@mui/icons-material/SmsOutlined';
-import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
-import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
-
-export default function WeblogPage() {
-    return (
-        <div className={"w-[85%] m-auto mt-12"}>
-            <section>
-                {/* side */}
-                <div className={" w-72 h-[367px] shadow-normal bg-white flex flex-col rounded-2xl p-4"}>
-                    <div className={' px-[13px] py-[10px]'}>
-                        <Link  href={"/"}>
-                            <TheaterComedyOutlinedIcon className={'ml-2'}/>
-                            <span>هنر و سینما</span>
-                        </Link>
-                    </div>
-                    <div className={' px-[13px] py-[10px]'}>
-                        <Link href={"/"}>
-                            <HomeWorkOutlinedIcon className={'ml-2'}/>
-                            <span>منتخب سردبیر</span>
-                        </Link>
-                    </div>
-                    <div  className={' px-[13px] py-[10px]'} >
-                        <Link href={"/"}>
-                            <MenuBookOutlinedIcon className={'ml-2'}/>
-                            <span>کتاب و ادبیات</span>
-                        </Link>
-                    </div>
-                    <div className={' px-[13px] py-[10px]'} >
-                        <Link href={"/"}>
-                            <PrecisionManufacturingOutlinedIcon className={'ml-2'}/>
-                            <span>علم و تکنولوژی</span>
-                        </Link>
-                    </div>
-                    <div className={' px-[13px] py-[10px]'}>
-                        <Link  href={"/"}>
-                            <SpaOutlinedIcon className={'ml-2'}/>
-                            <span>سبک زندگی</span>
-                        </Link>
-                    </div>
-                    <div className={' px-[13px] py-[10px]'} >
-                        <Link href={"/"}>
-                            <SportsEsportsOutlinedIcon className={'ml-2'}/>
-                            <span>بازی ویدئویی</span>
-                        </Link>
-                    </div>
-                    <div className={' px-[13px] py-[10px]'}>
-                        <Link href={"/"}>
-                            <ProductionQuantityLimitsOutlinedIcon className={'ml-2'}/>
-                            <span>آنباکس محصولات</span>
-                        </Link>
-                    </div>
-                </div>
-                {/* blog */}
-                <div className={''}>
-                    <div>
-                        <Image src={'../../../public/img/Blog/blog-new-7.jpg'} alt={"s"}
-                    </div>
-                </div>
-            </section>
-        </div>
-    )
+export default function weblog() {
+  return (
+    <div>weblog</div>
+  )
 }
+
---
+++ src/components/navbar.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+{/* FONT */}
+
+import {Amiri} from "next/font/google"
+
+const roboto = Amiri({
+    subsets: ["arabic"],
+    weight:["400"]
+})
+
+
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import StorefrontOutlinedIcon from "@mui/icons-material/StorefrontOutlined";
+import CoffeeIcon from "@mui/icons-material/Coffee";
+import FeedOutlinedIcon from "@mui/icons-material/FeedOutlined";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import ContactPhoneOutlinedIcon from "@mui/icons-material/ContactPhoneOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import ShoppingBasketOutlinedIcon from "@mui/icons-material/ShoppingBasketOutlined";
+
+const links = [
+    {url: "/", title: "Home"},
+    {url: "./account", title: "Account"},
+    {url: "./cart", title: "Cart"},
+    {url: "./shop", title: "Shop"},
+    {url: "./weblog", title: "Weblog"},
+    {url: "./order", title: "Order"},
+    {url: "./callUs", title: "CallUs"},
+    {url: "./aboutUS", title: "AboutUS"},
+];
+
+export default function navbar() {
+    return (
+        <div>
+            {/* HEADER */}
+            <div className=" rounded-[50px] bg-zinc-200 flex justify-around items-center  w-[85%] h-20 m-auto mt-4">
+                {/* nav and logo */}
+                <div className="flex items-center  group-hover:bg-sky-400">
+                    <Image src="./img/Logo.svg" alt="" width={50} height={50}/>
+                    <Link className={"px-4 transition-all hover:text-emerald-600"} href={"/"}>
+                        <HomeOutlinedIcon className={"text-emerald-600"}/>
+                        <span className={roboto.className}> صفحه اصلی </span>
+                    </Link>
+                    <Link className={"px-4 transition-all hover:text-emerald-600"} href={"/shop"}>
+                        <StorefrontOutlinedIcon className={"text-emerald-600"}/>
+                        <span className={roboto.className}> فروشگاه </span>
+                    </Link>
+                    <Link className={"px-4 transition-all hover:text-emerald-600"} href={"/order"}>
+                        <CoffeeIcon className={"text-emerald-600"}/>
+                        <span className={roboto.className}> سفارش قهوه </span>
+                    </Link>
+                    <Link className={"px-4 transition-all hover:text-emerald-600"} href={"/weblog"}>
+                        <FeedOutlinedIcon className={"text-emerald-600"}/>
+                        <span className={roboto.className}> وبلاگ </span>
+                    </Link>
+                    <Link className={"px-4 transition-all hover:text-emerald-600"} href={"/aboutUS"}>
+                        <InfoOutlinedIcon className={"text-emerald-600"}/>
+                        <span className={roboto.className}> درباره ما </span>
+                    </Link>
+                    <Link className={"px-4 transition-all hover:text-emerald-600"} href={"/callUs"}>
+                        <ContactPhoneOutlinedIcon className={"text-emerald-600"}/>
+                        <span className={roboto.className}> تماس با ما </span>
+                    </Link>
+                </div>
+
+                {/* search and btn */}
+                <div className=" flex items-center gap-5">
+                    <div className={"relative flex items-center"}>
+                        <SearchOutlinedIcon className={"absolute top-[11px] bottom-0 left-0 right-[90%]"}/>
+                        <input type="search" placeholder={"جستجو..."}
+                               className={"h-11 w-80 gap-y-2 pr-2 rounded-[20px]"}/>
+                    </div>
+                    <Link className={"text-white bg-yellow-950 p-2 rounded-full"} href={"/account"}>
+                        <PersonOutlinedIcon/>
+                    </Link>
+                    <Link className={"text-white bg-yellow-950 p-2 rounded-full"} href={"/cart"}>
+                        <ShoppingBasketOutlinedIcon/>
+                    </Link>
+                </div>
+            </div>
+            {/* MAIN */}
+        </div>
+    );
+}
