7cc726336117dc6b037f384fd3197b50e4cf531a
Mani hosseini <mani.hosseini.h8@gmail.com>
2024-05-15 16:52:14 +0330

    build section 2 of weblog page by mani

--- src/app/weblog/page.jsx
+++ src/app/weblog/page.jsx
@@ -18,11 +18,12 @@ import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
 export default function WeblogPage() {
     return (
         <div className={"w-[85%] m-auto mt-12"}>
-            <section>
+            {/* section 1 */}
+            <section className={'flex'}>
                 {/* side */}
-                <div className={" w-72 h-[367px] shadow-normal bg-white flex flex-col rounded-2xl p-4"}>
+                <div className={"ml-6 w-72 h-[367px] shadow-normal bg-white flex flex-col rounded-2xl p-4"}>
                     <div className={' px-[13px] py-[10px]'}>
-                        <Link  href={"/"}>
+                        <Link href={"/"}>
                             <TheaterComedyOutlinedIcon className={'ml-2'}/>
                             <span>هنر و سینما</span>
                         </Link>
@@ -33,25 +34,25 @@ export default function WeblogPage() {
                             <span>منتخب سردبیر</span>
                         </Link>
                     </div>
-                    <div  className={' px-[13px] py-[10px]'} >
+                    <div className={' px-[13px] py-[10px]'}>
                         <Link href={"/"}>
                             <MenuBookOutlinedIcon className={'ml-2'}/>
                             <span>کتاب و ادبیات</span>
                         </Link>
                     </div>
-                    <div className={' px-[13px] py-[10px]'} >
+                    <div className={' px-[13px] py-[10px]'}>
                         <Link href={"/"}>
                             <PrecisionManufacturingOutlinedIcon className={'ml-2'}/>
                             <span>علم و تکنولوژی</span>
                         </Link>
                     </div>
                     <div className={' px-[13px] py-[10px]'}>
-                        <Link  href={"/"}>
+                        <Link href={"/"}>
                             <SpaOutlinedIcon className={'ml-2'}/>
                             <span>سبک زندگی</span>
                         </Link>
                     </div>
-                    <div className={' px-[13px] py-[10px]'} >
+                    <div className={' px-[13px] py-[10px]'}>
                         <Link href={"/"}>
                             <SportsEsportsOutlinedIcon className={'ml-2'}/>
                             <span>بازی ویدئویی</span>
@@ -65,10 +66,104 @@ export default function WeblogPage() {
                     </div>
                 </div>
                 {/* blog */}
-                <div className={''}>
-                    <div>
-                        <Image src={'../../../public/img/Blog/blog-new-7.jpg'} alt={"s"}
+                <div className={"mr-6 w-full text-white"}>
+                    <div className={"flex justify-around gap-x-5 mb-5"}>
+                        <div className={"flex flex-col justify-end items-center w-[50%] h-60 custom-css rounded-2xl"}>
+                            <div className={'pb-3'}>
+                                <div className={"flex justify-center gap-x-5"}>
+                                    <div className={'text-xs'}>
+                                        <CalendarMonthOutlinedIcon className={'text-xs'}/>
+                                        <span> 16 مهر 1403 </span>
+                                    </div>
+                                    <div className={'text-xs'}>
+                                        <SmsOutlinedIcon className={'text-xs'}/>
+                                        <span> 2 دیدگاه </span>
+                                    </div>
+                                </div>
+                                <Link href={"/"} className={'text-base'}>
+                                    جدیدترین رندرهای شیائومی طراحی جذاب آن را نشان می‌دهند
+                                </Link>
+                            </div>
+                        </div>
+                        <div className={"flex flex-col justify-end items-center w-[50%] h-60 custom-css2 rounded-2xl"}>
+                            <div className={'pb-3'}>
+                                <div className={"flex justify-center gap-x-5"}>
+                                    <div className={'text-xs'}>
+                                        <CalendarMonthOutlinedIcon className={'text-xs'}/>
+                                        <span> 16 مهر 1403 </span>
+                                    </div>
+                                    <div className={'text-xs'}>
+                                        <SmsOutlinedIcon className={'text-xs'}/>
+                                        <span> 2 دیدگاه </span>
+                                    </div>
+                                </div>
+                                <Link href={"/"} className={'text-base'}>
+                                    رویداد رونمایی از سرفیس‌ها در تاریخ ۲۰ مهر برگزار می‌شود
+                                </Link>
+                            </div>
+                        </div>
                     </div>
+                    <div className={"flex justify-between"}>
+                        <div className={"flex flex-col justify-end items-center w-[360px] h-60 py-4 rounded-2xl custom-css3"}>
+                            <div className={'pb-3'}>
+                                <div className={"flex justify-center gap-x-5"}>
+                                    <div className={'text-xs'}>
+                                        <CalendarMonthOutlinedIcon className={'text-xs'}/>
+                                        <span> 16 مهر 1403 </span>
+                                    </div>
+                                    <div className={'text-xs'}>
+                                        <SmsOutlinedIcon className={'text-xs'}/>
+                                        <span> 2 دیدگاه </span>
+                                    </div>
+                                </div>
+                                <Link href={"/"} className={'text-base'}>
+                                    ۶ تکنولوژی که دیرتر از رقبا به آیفون آمده‌اند
+                                </Link>
+                            </div>
+                        </div>
+                        <div className={"flex flex-col justify-end items-center w-[360px] h-60 py-4 rounded-2xl custom-css4"}>
+                                                        <div className={'pb-3'}>
+                                <div className={"flex justify-center gap-x-5"}>
+                                    <div className={'text-xs'}>
+                                        <CalendarMonthOutlinedIcon className={'text-xs'}/>
+                                        <span> 16 مهر 1403 </span>
+                                    </div>
+                                    <div className={'text-xs'}>
+                                        <SmsOutlinedIcon className={'text-xs'}/>
+                                        <span> 2 دیدگاه </span>
+                                    </div>
+                                </div>
+                                <Link href={"/"} className={'text-base'}>
+                                    ۶ تکنولوژی که دیرتر از رقبا به آیفون آمده‌اند
+                                </Link>
+                            </div>
+                        </div>
+                        <div className={"flex flex-col justify-end items-center w-[360px] h-60 py-4 rounded-2xl custom-css5"}>
+                                                        <div className={'pb-3'}>
+                                <div className={"flex justify-center gap-x-5"}>
+                                    <div className={'text-xs'}>
+                                        <CalendarMonthOutlinedIcon className={'text-xs'}/>
+                                        <span> 16 مهر 1403 </span>
+                                    </div>
+                                    <div className={'text-xs'}>
+                                        <SmsOutlinedIcon className={'text-xs'}/>
+                                        <span> 2 دیدگاه </span>
+                                    </div>
+                                </div>
+                                <Link href={"/"} className={'text-base'}>
+                                    ۶ تکنولوژی که دیرتر از رقبا به آیفون آمده‌اند
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            { /* section 2 */}
+            <section className={'mt-24'}>
+                <div className={'flex items-center gap-x-2'}>
+                    <Image src={'/img/icon.svg'} alt={'dhd'} width={'38'} height={'38'}></Image>
+                    <h4 className={'text-4xl'}>مطالب وبلاگ</h4>
+                    <Image className={'mt-5'} src={'/img/brag.svg'} alt={'ee'} width={'36'} height={'36'}></Image>
                 </div>
             </section>
         </div>
